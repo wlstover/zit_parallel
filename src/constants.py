443756b4948c0937db4412ea42153df3281fd0a6
wlstover <wstover2@gmu.edu>
2026-02-13 13:16:35 -0500

    Updated analysis file viz

---
+++ src/constants.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+local_dir = Path("C:/Programming/papers/zi_paper/zit_parallel")
+station_dir = Path("C:/projects/zit_parallel")
+
+ROOT_DIR = local_dir
+OUTPUTS_DIR = ROOT_DIR / 'outputs'
+FIGURES_DIR = OUTPUTS_DIR / 'figures'
+
